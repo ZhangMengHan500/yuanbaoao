ff2f4aab70eee67c2cfe899031ab6229c7d70b88
zmh <3128089433@qq.com>
2026-06-05 09:50:43 +0800

    @ fix: 修复手机端无法使用的问题
    1. index.html 添加 ?v=2 破坏浏览器缓存，强制加载新 JS
    2. nginx 配置 HTML 不缓存，静态资源长期缓存
    3. ProfileStack.web.tsx 添加 Register 注册页面
    @

--- rn-app/src/navigation/ProfileStack.web.tsx
+++ rn-app/src/navigation/ProfileStack.web.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import ProfileScreen from '../screens/ProfileScreen';
+import RegisterScreen from '../screens/RegisterScreen';
 import RecordingListScreen from '../screens/RecordingListScreen';
 import RecordingDetailScreen from '../screens/RecordingDetailScreen';
 
@@ -14,6 +15,7 @@ const ProfileStack = () => {
         cardStyle: {backgroundColor: '#F5F5F5'},
       }}>
       <Stack.Screen name="ProfileHome" component={ProfileScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="RecordingList" component={RecordingListScreen} />
       <Stack.Screen name="RecordingDetail" component={RecordingDetailScreen} />
     </Stack.Navigator>
